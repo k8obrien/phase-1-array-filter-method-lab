e6d6c7160fb69022a16db0aeb20eefae551b00ac
kateobrien <k8obrien2811@gmail.com>
2021-09-29 22:35:58 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+function findMatching(array, variable){
+    const matchName = array.filter(element => element.toLowerCase() === variable.toLowerCase());
+    return matchName;
+}
+
+function fuzzyMatch(array, variable){
+    const matchLetters = array.filter(letters => letters.substring(0,2) === variable);
+    return matchLetters;
+}
+
+function matchName(array, variable){
+    return array.filter(element => element.name === variable)
+}
